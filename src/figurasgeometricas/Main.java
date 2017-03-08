dd5250861ae6969366b8639b67cb6b6f85b117eb
Toshiba <Toshiba@Lenovo1>
2017-03-07 19:21:03 -0500

    PROGRAMA DE FIGURAS GEOMETRICAS COMPLETO....

---
+++ src/figurasgeometricas/Main.java
@@ -0,0 +1,17 @@
+
+package figurasgeometricas;
+
+/**
+ * Clase por donde arranca el programa 
+ * @author Michael Cardenas
+ * @author Edgar Krejci
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        /**
+         * aqui se crea un objeto de la clase FigurasGeometricas
+         */
+        FigurasGeometricas figurasGeometricas = new FigurasGeometricas();                
+    }    
+}
